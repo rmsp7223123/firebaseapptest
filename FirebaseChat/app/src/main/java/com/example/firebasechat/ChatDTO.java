22f4a7b27cac6d97f9f6d11e5e594a73ec07a579
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-07-02 21:26:33 +0900

    채팅기능 구현

---
+++ FirebaseChat/app/src/main/java/com/example/firebasechat/ChatDTO.java
@@ -0,0 +1,28 @@
+package com.example.firebasechat;
+
+public class ChatDTO {
+
+    private String userName;
+    private String message;
+
+    public ChatDTO(String userName, String message) {
+        this.userName = userName;
+        this.message = message;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
