5ce4f3b973786f1503e5cbf16321f4a5b8c91c59
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-07-02 16:21:37 +0900

    파이어 베이스 이메일로 로그인 만들기 수정끝

--- FirebaseRegister/app/src/main/java/com/example/firebaseregister/RegisterActivity.java
+++ FirebaseRegister/app/src/main/java/com/example/firebaseregister/RegisterActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -58,6 +59,7 @@ public class RegisterActivity extends AppCompatActivity {
                         mDatabaseRef.child("UserAccount").child(firebaseUser.getUid()).setValue(account);
                         Toast.makeText(RegisterActivity.this, "회원가입에 성공했습니다.", Toast.LENGTH_SHORT).show();
                     } else {
+                        Log.d("확인", "onComplete: " + task);
                         Toast.makeText(RegisterActivity.this, "회원가입에 실패했습니다.", Toast.LENGTH_SHORT).show();
                     }
                 }
